df09c2a8eecca631df3f50d5b07bf5c8420f6cfe
Murilo Lima <murilo.almeidalima@outlook.com>
2018-11-09 17:38:32 -0200

    adicionado login

--- Sistema evolution/SistemaEvolution/Apresentacao/MainWindow.xaml.cs
+++ Sistema evolution/SistemaEvolution/Apresentacao/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SistemaEvolution;
 
 namespace SistemaEvolution
 {
@@ -27,6 +28,16 @@ namespace SistemaEvolution
 
         private void btnLogar_Click(object sender, RoutedEventArgs e)
         {
+            var login = new Modelo.Login();
+            string ID_Usuario;
+            string Senha;
+            ID_Usuario = txbLogin.Text;
+            Senha = txbSenha.Text;
+            if (!login.Logon(ID_Usuario, Senha))
+            {
+                MessageBox.Show("Login ou senha Incorretos");
+            }
+
             Apresentacao.frmPaginaPrincipal frmC = new Apresentacao.frmPaginaPrincipal();
             frmC.ShowDialog();
         }
---
+++ Sistema evolution/SistemaEvolution/Modelo/Login.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaEvolution;
+
+namespace SistemaEvolution.Modelo
+{
+     public class Login
+    {
+        public bool Logon(String ID_Usuario, String Senha)
+        {
+            var db = new EvolutionEntities();
+            var usuario = db.Usuario.FirstOrDefault(u => u.ID_usuario == ID_Usuario && u.Senha == Senha);
+            if (usuario != null)
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
